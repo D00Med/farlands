d860ca8ef763666c961e7d88db086df4991384a1
D00Med <heiselong@gmx.com>
2017-01-19 05:51:30 +1000

    scarecrow

--- mods/decoblocks/nodes.lua
+++ mods/decoblocks/nodes.lua
@@ -1,5 +1,3 @@
-decoblocks = {}
-
 minetest.register_node("decoblocks:old_stone_tiles", {
 	description = "Old Stone Tiles",
 	tiles = {"decoblocks_old_stone_tiles.png"},
@@ -12,4 +10,27 @@ minetest.register_node("decoblocks:old_stone_tiles_with_dirt", {
 	tiles = {"decoblocks_old_stone_tiles_with_dirt.png"},
 	groups = {cracky = 3, stone = 2},
 	sounds = default.node_sound_stone_defaults(),
+})
+
+
+minetest.register_node("decoblocks:scarecrow", {
+	description = "Scarecrow",
+	drawtype = "mesh",
+	mesh = "scarecrow.obj",
+	tiles = {
+		"decoblocks_scarecrow.png",
+	},
+	visual_scale = 0.5,
+	wield_scale = {x=0.5, y=0.5, z=0.5},
+	paramtype = "light",
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 1, 0.3}
+	},
+	collision_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 1, 0.3}
+	},
+	groups = {choppy = 1, oddly_breakable_by_hand = 1},
+	sounds = default.node_sound_wood_defaults()
 })
