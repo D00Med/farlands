b3e29218ac28b4dd4e77e30502deb1276c06ba1f
D00Med <heiselong@gmx.com>
2017-01-22 08:55:32 +1000

    new willow tree

--- mods/mapgen/mapgen.lua
+++ mods/mapgen/mapgen.lua
@@ -1452,6 +1452,25 @@ end
 		flags = "place_center_x, place_center_z",
 	})
 	
+	minetest.register_decoration({
+		deco_type = "schematic",
+		place_on = {"mapgen:dirt_with_swampgrass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0.0,
+			scale = -0.015,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"swamp"},
+		y_min = 1,
+		y_max = 31000,
+		schematic = minetest.get_modpath("mapgen") .. "/schematics/willow2.mts",
+		flags = "place_center_x, place_center_z",
+	})
+	
 	-- Aspen tree and log
 
 	minetest.register_decoration({
