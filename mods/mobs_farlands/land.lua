47463b59104f1d4904e2df49c32148b9b24a4b6e
D00Med <heiselong@gmx.com>
2017-02-04 09:46:10 +1000

    swamp lurker

--- mods/mobs_farlands/land.lua
+++ mods/mobs_farlands/land.lua
@@ -249,3 +249,48 @@ mobs:register_mob("mobs_m:zombie", {
 
 mobs:register_egg("mobs_m:zombie", "Zombie", "default_dirt.png", 1)
 mobs:register_spawn("mobs_m:zombie", {"default:dirt_with_grass",}, 5, 0, 7000, 0, 11000)
+
+mobs:register_mob("mobs_m:swamp_lurker", {
+	type = "monster",
+	passive = false,
+	reach = 2,
+	damage = 1,
+	attack_type = "dogfight",
+	hp_min = 30,
+	hp_max = 40,
+	armor = 120,
+	collisionbox = {-0.35, 0,-0.35, 0.35,1.8,0.35},
+	visual = "mesh",
+	mesh = "swamplurker.b3d",
+	textures = {
+		{"mobs_swamplurker.png"},
+	},
+	blood_texture = "default_dirt.png",
+	makes_footstep_sound = true,
+	walk_velocity = 1,
+	run_velocity = 1.5,
+	jump = 1,
+	water_damage = 0,
+	lava_damage = 2,
+	light_damage = 0,
+	fall_damage = 1,
+	view_range = 10,
+	drops = {
+		{name = "mobs:zombie_flesh_raw", chance = 2, min = 1, max = 2},
+	},
+	animation = {
+		speed_normal = 14,
+		speed_run = 18,
+		stand_start = 1,
+		stand_end = 25,
+		walk_start = 55,
+		walk_end = 75,
+		run_start = 55,
+		run_end = 75,
+		punch_start = 20,
+		punch_end = 50,
+	},
+})
+
+mobs:register_egg("mobs_m:swamp_lurker", "Swamp Lurker", "default_dirt.png", 1)
+mobs:register_spawn("mobs_m:swamp_lurker", {"default:dirt_with_swamp_grass",}, 5, 0, 7000, 0, 11000)
