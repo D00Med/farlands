6ed218225819347c1d91ddf6ce14d9bd58c1d081
D00Med <heiselong@gmx.com>
2017-02-13 10:33:32 +1000

    I have a goldfish brain

--- mods/mapgen/mapgen.lua
+++ mods/mapgen/mapgen.lua
@@ -1445,6 +1445,25 @@ end
 		schematic = minetest.get_modpath("mapgen") .. "/schematics/bamboo2.mts",
 		flags = "place_center_x, place_center_z",
 	})
+	
+				minetest.register_decoration({
+		deco_type = "schematic",
+		place_on = {"default:dirt_with_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0.016,
+			scale = 0.012,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"bamboo_forest",},
+		y_min = 1,
+		y_max = 31000,
+		schematic = minetest.get_modpath("mapgen") .. "/schematics/bamboo5.mts",
+		flags = "place_center_x, place_center_z",
+	})
 
 	-- Apple tree and log
 
