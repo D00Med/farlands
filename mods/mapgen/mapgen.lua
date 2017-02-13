042ea24d906960381722bd3e8c5eb19ee6597d2c
D00Med <heiselong@gmx.com>
2017-02-13 11:01:57 +1000

    baobab trees

--- mods/mapgen/mapgen.lua
+++ mods/mapgen/mapgen.lua
@@ -1689,8 +1689,8 @@ end
 		place_on = {"default:dirt_with_dry_grass"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0,
-			scale = 0.002,
+			offset = -0.05,
+			scale = 0.001,
 			spread = {x = 250, y = 250, z = 250},
 			seed = 2,
 			octaves = 3,
@@ -1700,6 +1700,46 @@ end
 		y_min = 1,
 		y_max = 31000,
 		schematic = minetest.get_modpath("mapgen") .. "/schematics/baobab.mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
+	
+			minetest.register_decoration({
+		deco_type = "schematic",
+		place_on = {"default:dirt_with_dry_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = -0.001,
+			scale = 0.001,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"savanna"},
+		y_min = 1,
+		y_max = 31000,
+		schematic = minetest.get_modpath("mapgen") .. "/schematics/baobab1.mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
+	
+			minetest.register_decoration({
+		deco_type = "schematic",
+		place_on = {"default:dirt_with_dry_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = -0.001,
+			scale = 0.001,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"savanna"},
+		y_min = 1,
+		y_max = 31000,
+		schematic = minetest.get_modpath("mapgen") .. "/schematics/baobab2.mts",
 		flags = "place_center_x, place_center_z",
 		rotation = "random",
 	})
