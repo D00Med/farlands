dab71d4fc065e6158c2cb1277fcacd364646e886
Toby plowy <toby123@live.nl>
2017-07-03 01:38:18 +0200

    tools

--- mods/backpack/crafts.lua
+++ mods/backpack/crafts.lua
@@ -2,7 +2,7 @@ minetest.register_craft({
 	output = 'backpack:backpack',
 	recipe = {
 		{'group:wool', 'group:wool', 'group:wool'},
-		{'group:wool', 'group:wool', 'group:wool'},
+		{'group:wool', 'default:chest', 'group:wool'},
 		{'group:wool', 'group:wool', 'group:wool'},
 	}
 })
