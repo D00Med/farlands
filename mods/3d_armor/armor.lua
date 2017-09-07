2094adebc654815b3507c9de2c887f6ff9cc8c18
D00Med <heiselong@gmx.com>
2017-09-07 19:23:07 +1000

    2x2 crafting grid

--- mods/3d_armor/armor.lua
+++ mods/3d_armor/armor.lua
@@ -52,13 +52,19 @@ if not minetest.get_modpath("ethereal") then
 	ARMOR_MATERIALS.crystal = nil
 end
 
+minetest.register_on_joinplayer(function(player)
+	local inv = player:get_inventory()
+	inv:set_size("craft", 4)
+	inv:set_width("craft", 2)
+end)
+
 armor = {
 	timer = 0,
 	elements = {"head", "torso", "legs", "feet"},
 	physics = {"jump","speed","gravity"},
 	formspec = "size[8,8.5]image[2,0.75;2,4;armor_preview]"
 		.."list[current_player;main;0,4.5;8,4;]"
-		.."list[current_player;craft;4,1;3,3;]"
+		.."list[current_player;craft;4,1;2,2;]"
 		.."list[current_player;craftpreview;7,2;1,1;]"
 		.."listring[current_player;main]"
 		.."listring[current_player;craft]",
