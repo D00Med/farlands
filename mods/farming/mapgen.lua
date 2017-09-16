41ee64241f3ed60f14345dbee4a31f2f5f28add1
D00Med <heiselong@gmx.com>
2017-09-16 20:48:49 +1000

    fiixes

--- mods/farming/mapgen.lua
+++ mods/farming/mapgen.lua
@@ -20,23 +20,23 @@ local function register_plant(name, min, max, spawnby, num)
 end
 
 function farming.register_mgv6_decorations()
-	register_plant("potato_3", 15, 40, "", -1)
+	register_plant("potato_4", 15, 40, "", -1)
 	register_plant("raddish_4", 5, 20, "", -1)
-	register_plant("carrot_8", 1, 30, "group:water", 1)
-	register_plant("corn_7", 12, 22, "", -1)
-	register_plant("corn_8", 10, 20, "", -1)
-	register_plant("melon_8", 1, 20, "group:water", 1)
-	register_plant("pumpkin_8", 1, 20, "group:water", 1)
+	register_plant("carrot_4", 1, 30, "group:water", 1)
+	register_plant("corn_4", 12, 22, "", -1)
+	register_plant("corn_4", 10, 20, "", -1)
+	register_plant("melon_4", 1, 20, "group:water", 1)
+	register_plant("pumpkin_4", 1, 20, "group:water", 1)
 end
 
 -- v7 maps have a beach so plants growing near water is limited to 6- high
 function farming.register_mgv7_decorations()
-	register_plant("potato_3", 15, 40, "", -1)
+	register_plant("potato_4", 15, 40, "", -1)
 	register_plant("raddish_4", 5, 20, "", -1)
-	register_plant("carrot_8", 1, 6, "", -1)
-	register_plant("corn_7", 12, 22, "", -1)
-	register_plant("corn_8", 10, 20, "", -1)
-	register_plant("melon_8", 1, 6, "", -1)
-	register_plant("pumpkin_8", 1, 6, "", -1)
+	register_plant("carrot_4", 1, 6, "", -1)
+	register_plant("corn_4", 12, 22, "", -1)
+	register_plant("corn_4", 10, 20, "", -1)
+	register_plant("melon_4", 1, 6, "", -1)
+	register_plant("pumpkin_4", 1, 6, "", -1)
 end
 
