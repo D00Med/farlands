72884bf11f09926daca41faf92a2ccd3ce20a25a
D00Med <heiselong@gmx.com>
2017-11-12 12:50:37 +1000

    Stained glass

--- mods/decoblocks/nodes.lua
+++ mods/decoblocks/nodes.lua
@@ -1245,3 +1245,41 @@ minetest.register_node("decoblocks:polar_bear_rug", {
 	groups = {cracky = 2, oddly_breakable_by_hand = 1},
 	sounds = default.node_sound_stone_defaults(),
 })
+
+--stained glass
+
+local colours = {
+{"red", "Red"},
+{"green", "Green"},
+{"blue", "Blue"},
+{"yellow", "Yellow"},
+{"cyan", "Cyan"},
+{"brown", "Brown"},
+{"orange", "Orange"},
+{"purple", "Purple"},
+{"magenta", "Magenta"},
+{"darkgreen", "Dark Green"},
+{"darkgrey", "Dark Grey"},
+{"grey", "Grey"},
+{"white", "White"},
+{"black", "Black"},
+{"pink", "Pink"},
+}
+
+for _, row in ipairs(colours) do
+local colour = row[1]
+local desc = row[2]
+xpanes.register_pane(""..colour.."_glass", {
+	description = ""..desc.." Stained Glass Pane",
+	textures = {"decoblocks_"..colour.."_glass.png","decoblocks_"..colour.."_glass.png","decoblocks_"..colour.."_glass.png"},
+	use_texture_alpha = true,
+	inventory_image = "decoblocks_"..colour.."_glass.png",
+	wield_image = "decoblocks_"..colour.."_glass.png",
+	sounds = default.node_sound_glass_defaults(),
+	groups = {snappy=2, cracky=3, oddly_breakable_by_hand=3},
+	recipe = {
+		{"", "dye:"..colour, ""},
+		{"", "xpanes:pane", ""}
+	}
+})
+end
